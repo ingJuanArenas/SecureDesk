2e7000f4a6608fdea874b06d4ce63c1cc5ee29ad
ingJuanArenas <juanarenasing@gmail.com>
2026-04-09 15:41:10 -0500

    initial commit

---
+++ src/main/java/com/secure_desk/SecureDeskApplication.java
@@ -0,0 +1,13 @@
+package com.secure_desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secure_desk/SecureDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure_desk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
