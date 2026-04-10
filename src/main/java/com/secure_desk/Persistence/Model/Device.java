0850331e58d8619622896d82776c7bd21880e934
ingJuanArenas <juanarenasing@gmail.com>
2026-04-10 15:40:20 -0500

    protecting serial number

--- src/main/java/com/secure_desk/Persistence/Model/Device.java
+++ src/main/java/com/secure_desk/Persistence/Model/Device.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -19,6 +21,9 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Device {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(name = "serial_number", nullable = false, unique = true, updatable = false)
     private String serialNumber;
 
